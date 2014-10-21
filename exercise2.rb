8fded74c5357897d04c48a38997761415984c263
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-21 09:35:18 -0400

    Updated exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,25 @@
+#1. Cost of meal with tip
+
+costOfMeal = 55
+tip = 0.17
+
+totalCost = costOfMeal + (tip * costOfMeal)
+
+puts "Cost of meal: #{totalCost}"
+
+#2. String + integer
+
+sentence = 12.to_s + " Monkeys"
+
+puts sentence
+
+#3. Multiplication
+
+a = 45628
+b = 7839
+
+puts "#{a} x #{b} = #{a*b}"
+
+#4. True or false
+
+puts (true && false) || (false && true) || !(false && false) #This is true!
